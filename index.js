ea42724c0e5c30fb8c18a7f2ef3c1b973095f079
Jesús Aguilar <j.luukvg@gmail.com>
2018-01-23 19:15:55 -0600

    style: Remove unnecesary code
    Code rendered unnecesary from the previous test is deleted.

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 var engine = require('./engine');
 var luxboxCommitTypes = require('./luxbox-commit-types.json');
-var conventionalCommitTypes = require('conventional-commit-types');
 
 module.exports = engine({
   types: luxboxCommitTypes.types
