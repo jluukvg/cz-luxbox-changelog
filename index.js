f421699a11161f1f9d25e2ea2b1e7a66c6dbd55f
Jesús Aguilar <j.luukvg@gmail.com>
2018-01-23 19:13:04 -0600

    test(index.js): Add a local .json file with the LuxBox commit types
    In order to quickly modify the commit types locally, a local .json file
    will be used from now on. This commit is used to test it.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 "format cjs";
 
 var engine = require('./engine');
+var luxboxCommitTypes = require('./luxbox-commit-types.json');
 var conventionalCommitTypes = require('conventional-commit-types');
 
 module.exports = engine({
-  types: conventionalCommitTypes.types
+  types: luxboxCommitTypes.types
 });
